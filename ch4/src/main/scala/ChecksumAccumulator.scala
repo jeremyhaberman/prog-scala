d3f8dc8ea39905275da951410d510283e399e6b7
Jeremy Haberman <jeremy.haberman@drip.com>
2022-09-07 08:41:24 -0500

    4.3

--- ch4/src/main/scala/ChecksumAccumulator.scala
+++ ch4/src/main/scala/ChecksumAccumulator.scala
@@ -7,3 +7,22 @@ class ChecksumAccumulator {
 
   def rawsum(): Int = sum
 }
+
+import scala.collection.mutable.Map
+
+object ChecksumAccumulator {
+  private val cache = Map[String, Int]()
+
+  def calculate(s: String): Int =
+    if (cache.contains(s))
+      cache(S)
+    else {
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+
+}
