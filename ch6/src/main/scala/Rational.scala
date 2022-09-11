a7fbcd813a83c9e53844dc06e15db41610d3ab19
Jeremy Haberman <jeremy.haberman@drip.com>
2022-09-11 18:05:38 -0500

    6.8

--- ch6/src/main/scala/Rational.scala
+++ ch6/src/main/scala/Rational.scala
@@ -1,8 +1,9 @@
 class Rational(n: Int, d: Int) {
     require(d != 0, "d cannot be 0")
 
-    val numerator: Int = n
-    val denominator: Int = d
+    private val g = gcd(n.abs, d.abs)
+    val numerator: Int = n / g
+    val denominator: Int = d / g
 
     def this(n: Int) = this(n, 1)
     
@@ -19,4 +20,7 @@ class Rational(n: Int, d: Int) {
         this.numerator * that.denominator < that.numerator * this.denominator
 
     def max(that: Rational) = if (lessThan(that)) that else this
+
+    private def gcd(a: Int, b: Int): Int = 
+        if (b == 0) a else gcd(b, a % b)
 }
