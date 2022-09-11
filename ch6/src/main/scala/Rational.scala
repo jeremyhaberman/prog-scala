13ce4e300df60b77475f6454793d8daaddb11709
Jeremy Haberman <jeremy.haberman@drip.com>
2022-09-11 18:28:38 -0500

    6.11

--- ch6/src/main/scala/Rational.scala
+++ ch6/src/main/scala/Rational.scala
@@ -11,12 +11,31 @@ class Rational(n: Int, d: Int) {
 
     def +(that: Rational): Rational =
         new Rational(
-            numerator * that.denominator + that.numerator * denominator,
+            (numerator * that.denominator) + (that.numerator * denominator),
             denominator * that.denominator
         )
+    
+    def +(i: Int): Rational = 
+        new Rational(numerator + (i * denominator), denominator)
+    
+    def -(that: Rational): Rational = 
+        new Rational(
+            (numerator * that.denominator) - (that.numerator * denominator),
+            denominator * that.denominator
+        )
+    
+    def -(i: Int): Rational = 
+        new Rational(numerator - (i * denominator), denominator)
 
     def *(that: Rational): Rational = 
         new Rational(numerator * that.numerator, denominator * that.denominator)
+
+    def *(i: Int): Rational = new Rational(numerator * i, denominator)
+
+    def /(that: Rational): Rational = 
+        new Rational(numerator * that.denominator, denominator * that.numerator)
+
+    def /(i: Int): Rational = new Rational(numerator, denominator * i)
 
     def lessThan(that: Rational): Boolean = 
         this.numerator * that.denominator < that.numerator * this.denominator
