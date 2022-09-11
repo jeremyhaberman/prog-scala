7950f7e63b481a1566338ddae0115369f1d950cf
Jeremy Haberman <jeremy.haberman@drip.com>
2022-09-11 17:44:52 -0500

    6.5

--- ch6/src/main/scala/Rational.scala
+++ ch6/src/main/scala/Rational.scala
@@ -1,4 +1,15 @@
 class Rational(n: Int, d: Int) {
     require(d != 0, "d cannot be 0")
-    override def toString = n + "/" + d
+
+    val numerator: Int = n
+    val denominator: Int = d
+    
+    override def toString = numerator + "/" + denominator
+
+    def add(that: Rational): Rational = {
+        new Rational(
+            numerator * that.denominator + that.numerator * denominator,
+            denominator * that.denominator
+        )
+    }
 }
